cbbf41cc876bd69f42779e2c50671dfc69b23ef1
amanhappysingh <amans.happysingh@gmail.com>
2026-02-06 20:38:41 +0530

    all new

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50"
+      className="bg-blue-600 bg-blue-600  text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50"
     >
       {children}
     </button>
